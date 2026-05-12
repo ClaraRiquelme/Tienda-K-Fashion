9e5d5dc8cbe23a561324ab3f4ca9293335ac5465
Naitomeru <ronnaitomeru@gmail.com>
2026-05-12 00:40:35 -0400

    Añadido modificaciones a carrito
    Se puede:
    - Quitar item
    - Quitar todos los items
    - Comprar todo (misma funcionalidad que "Quitar todo")

--- script.js
+++ script.js
@@ -92,14 +92,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const container = galerias[1];
     const original_item = container.children[0];
     original_item.remove();
-    console.log(original_item);
 
     for (const element of db) {
-        console.log(element.nombre);
 
         const item = original_item.cloneNode(true);
+
+        // Imagen
         item.children[0].children[0].src = element.imagen;
+
+        // Nombre ropa
         item.children[1].innerHTML = element.nombre;
+
+        // Precio
         const precio_number = element.precio.toString();
         let count = 0;
         const precio1 = precio_number.substring(count, precio_number.length % 3);
@@ -107,12 +111,14 @@ document.addEventListener("DOMContentLoaded", function() {
         const precio2 = precio_number.substring(count);
         item.children[2].innerHTML = "$" + precio1 + "." + precio2 + " CLP";
 
+        // Callback botón
         item.children[3].addEventListener("click", function(event) {
             event.preventDefault();
             const cart = JSON.parse( localStorage.getItem("cart") ) || [];
 
             let exists = false;
             for (const item_in_cart of cart) {
+                // Si ya está en carrito, aumentar cantidad
                 if (item_in_cart.id === element.id) {
                     item_in_cart.cantidad += 1;
                     exists = true;
@@ -144,6 +150,8 @@ document.addEventListener("DOMContentLoaded", function() {
 // carrito.html
 document.addEventListener("DOMContentLoaded", function() {
     const carrito_main = document.getElementsByClassName("carrito-main");
+
+    // Si no hay carrito-main, no estamos en carrito.html
     if (carrito_main.length === 0) return;
 
     const container = carrito_main[0].children[0]
@@ -151,27 +159,63 @@ document.addEventListener("DOMContentLoaded", function() {
     original_item.remove();
 
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
-    console.log("Cart:");
-    console.log(cart);
 
-    for (const item_in_cart of cart) {
-        console.log("Nombre: " + item_in_cart.nombre + " Cantidad: " + item_in_cart.cantidad);
-        const item = original_item.cloneNode(true);
+    if (cart.length === 0) {
+        container.remove();
 
-        item.children[0].children[0].src = item_in_cart.imagen;
-        item.children[1].children[0].innerHTML = item_in_cart.nombre;
-        const precio_number = item_in_cart.precio.toString();
-        let count = 0;
-        const precio1 = precio_number.substring(count, precio_number.length % 3);
-        count += precio_number.length % 3;
-        const precio2 = precio_number.substring(count);
-        item.children[1].children[1].innerHTML = "$" + precio1 + "." + precio2 + " CLP";
-        item.children[1].children[2].innerHTML = "Cantidad: " + item_in_cart.cantidad;
+        carrito_main[0].innerHTML = "Tu carrito está vacío"
+    } else {        
+        for (const item_in_cart of cart) {
+            const item = original_item.cloneNode(true);
+    
+            // Imagen
+            const item_image = item.children[0];
+            item_image.children[0].src = item_in_cart.imagen;
+    
+            const item_detalles = item.children[1];
+    
+            // Nombre ropa
+            item_detalles.children[0].innerHTML = item_in_cart.nombre;
+    
+            // Precio
+            const precio_number = item_in_cart.precio.toString();
+            let count = 0;
+            const precio1 = precio_number.substring(count, precio_number.length % 3);
+            count += precio_number.length % 3;
+            const precio2 = precio_number.substring(count);
+            item_detalles.children[1].innerHTML = "$" + precio1 + "." + precio2 + " CLP";
+    
+            // Cantidad
+            item_detalles.children[2].innerHTML = "Cantidad: " + item_in_cart.cantidad;
+    
+            container.append(item);
+        }
+    
+        const buttons_remove = Array.from(document.getElementsByClassName("remove-button"));
+        
+        buttons_remove.forEach((btn, index) => {
+            btn.addEventListener("click", function(event) {
+                cart.splice(index, 1);
+                localStorage.setItem("cart", JSON.stringify(cart));
+                location.reload();
+            })
+        })
 
-        container.append(item);
+        const button_remove_all = document.getElementsByClassName("remove-all-button")[0];
+        button_remove_all.addEventListener("click", function() {
+            localStorage.removeItem("cart");
+            location.reload();
+        })
+
+        const button_purchase_all = document.getElementsByClassName("purchase-all-button")[0];
+        button_purchase_all.addEventListener("click", function() {
+            localStorage.removeItem("cart");
+            location.reload();
+        })
     }
 });
 
+// Solo para debug
 document.addEventListener("keydown", function(event) {
     if (event.key === "d") {
         localStorage.removeItem("cart");
