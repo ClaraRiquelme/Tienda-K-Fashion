2f970c5967fedde135eabf356b07fb77791c7660
Naitomeru <ronnaitomeru@gmail.com>
2026-05-12 22:28:50 -0400

    Añadido dinamismo a la página detalles.html
    El botón "Ver más" de las tarjetas ahora redireccionan
    a la página detalles.html con el id de la ropa como parámetro.
    
    También, se refactorizó un poco, añadiendo funciones y comentarios.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Global variables
 const addToCartButtonClass = "boton";
-// "Colección" de "Base de datos" con las características de cada ropa
+
+/**
+ * La colección de ropas
+ */
 const db = [
     {
         "id": 1,
@@ -84,87 +87,145 @@ const db = [
     }
 ]
 
+/**
+ * Formatea el precio para mostrarlo en pesos chilenos.
+ *
+ * @param {number} precio El precio a formatear
+ * @returns {string} El precio formateado
+ */
+function precioToString(precio) {
+    const precio_number = precio.toString();
+    let count = 0;
+    const precio1 = precio_number.substring(count, precio_number.length % 3);
+    count += precio_number.length % 3;
+    const precio2 = precio_number.substring(count);
+    return "$" + precio1 + "." + precio2 + " CLP";
+}
+
+/**
+ * Función para añadir un producto al carrito de compras.
+ * 
+ * @param {Object} product 
+ * @param {HTMLElement} cart_quantity_text 
+ */
+function addToCart(product, cart_quantity_text) {
+    // Se obtiene el carrito desde la memoria
+    // Si no existe, se pasa un arreglo vacío
+    const cart = JSON.parse( localStorage.getItem("cart") ) || [];
+
+    let exists = false;
+    for (const item_in_cart of cart) {
+        // Si ya está en carrito, aumentar cantidad
+        if (item_in_cart.id === product.id) {
+            item_in_cart.cantidad += 1;
+            exists = true;
+            break;
+        }
+    }
+    
+    // Si no existe el producto, añadirlo al carrito
+    if (!exists) {
+        cart.push(product)
+        console.log(cart);
+
+        // Actualiza la cantidad de elementos del carrito
+        cart_quantity_text.innerHTML = "(" + cart.length + ")";
+    }
+
+    // Actualiza el carrito en la memoria
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+/**
+ * Asigna la cantidad total de elementos del carrito al ícono superior derecho.
+ */
+function setCartQuantity(cart_quantity_text) {
+    // Se consigue el texto y se asigna el total de elementos del carrito
+    const cart = JSON.parse( localStorage.getItem("cart") ) || [];
+    cart_quantity_text.innerHTML = "(" + cart.length + ")";
+}
+
 // index.html
 document.addEventListener("DOMContentLoaded", function() {
+    // Si no hay elementos de clase "galeria", salimos de la función prematuramente
     const galerias = document.getElementsByClassName("galeria");
-    if (galerias.length === 0) return;
+    if (galerias.length < 2) return;
 
+    // Conseguimos el contenedor de las tarjetas
+    // y la tarjeta de plantilla
     const container = galerias[1];
     const original_item = container.children[0];
     original_item.remove();
 
+    // Se consigue el texto de la cantidad de elementos del carrito
+    const cart_quantity_text = document.getElementsByClassName("carrito")[0].children[0].children[1];
+    setCartQuantity(cart_quantity_text);
+
+    // Añadimos las tarjetas dinámicamente
     for (const element of db) {
 
+        // Clonamos la tarjeta plantilla
+        // y le asignamos los valores correspondientes
         const item = original_item.cloneNode(true);
 
         // Imagen
         item.children[0].children[0].src = element.imagen;
 
+        // Se asigna el link del botón "Ver más"
+        // al URL de detalles.html con el id del elemento como parámetro
+        // para que la página de detalles muestre los datos de este elemento
+        item.children[0].children[1].href = "detalles.html?id=" + element.id;
+
         // Nombre ropa
         item.children[1].innerHTML = element.nombre;
 
         // Precio
-        const precio_number = element.precio.toString();
-        let count = 0;
-        const precio1 = precio_number.substring(count, precio_number.length % 3);
-        count += precio_number.length % 3;
-        const precio2 = precio_number.substring(count);
-        item.children[2].innerHTML = "$" + precio1 + "." + precio2 + " CLP";
+        item.children[2].innerHTML = precioToString(element.precio);
 
-        // Callback botón
+        // Se añade un callback al botón de "Agregar al carrito"
         item.children[3].addEventListener("click", function(event) {
-            event.preventDefault();
-            const cart = JSON.parse( localStorage.getItem("cart") ) || [];
+            // event.preventDefault();
 
-            let exists = false;
-            for (const item_in_cart of cart) {
-                // Si ya está en carrito, aumentar cantidad
-                if (item_in_cart.id === element.id) {
-                    item_in_cart.cantidad += 1;
-                    exists = true;
-                    break;
-                }
+            // Creamos el objeto con los datos importantes
+            // para insertarlo en el carrito
+            // TODO: agregar talla y diferenciarlo de otro elemento
+            // del mismo tipo en el carrito
+            const item_to_push = {
+                "id": element.id,
+                "nombre": element.nombre,
+                "precio": element.precio,
+                "imagen": element.imagen,
+                "cantidad": 1
             }
-            
-            if (!exists) {
-                item_to_push = {
-                    "id": element.id,
-                    "nombre": element.nombre,
-                    "precio": element.precio,
-                    "descripcion": element.descripcion,
-                    "tallas": element.tallas,
-                    "imagen": element.imagen,
-                    "cantidad": 1
-                }
-                cart.push(item_to_push)
-                console.log(cart);
-            }
-
-            localStorage.setItem("cart", JSON.stringify(cart));
+            addToCart(item_to_push, cart_quantity_text);
         })
 
+        // Se añade la nueva tarjeta al contenedor/galería
         container.append(item);
     }
 });
 
 // carrito.html
 document.addEventListener("DOMContentLoaded", function() {
+    // Si no hay carrito-main, no estamos en carrito.html y salimos de la función prematuramente
     const carrito_main = document.getElementsByClassName("carrito-main");
-
-    // Si no hay carrito-main, no estamos en carrito.html
     if (carrito_main.length === 0) return;
 
+    // Conseguimos el contenedor de las tarjetas
+    // y la tarjeta de plantilla
     const container = carrito_main[0].children[0]
     const original_item = container.children[0];
     original_item.remove();
 
+    // Conseguimos el carrito desde la memoria
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-    if (cart.length === 0) {
+    if (cart.length === 0) { // Si el carrito está vacío, se muestra mensaje acorde.
         container.remove();
 
         carrito_main[0].innerHTML = "Tu carrito está vacío"
-    } else {        
+    } else {  
+        // Por cada elemento del carrito, se crea una tarjeta      
         for (const item_in_cart of cart) {
             const item = original_item.cloneNode(true);
     
@@ -178,12 +239,7 @@ document.addEventListener("DOMContentLoaded", function() {
             item_detalles.children[0].innerHTML = item_in_cart.nombre;
     
             // Precio
-            const precio_number = item_in_cart.precio.toString();
-            let count = 0;
-            const precio1 = precio_number.substring(count, precio_number.length % 3);
-            count += precio_number.length % 3;
-            const precio2 = precio_number.substring(count);
-            item_detalles.children[1].innerHTML = "$" + precio1 + "." + precio2 + " CLP";
+            item_detalles.children[1].innerHTML = precioToString(item_in_cart.precio);
     
             // Cantidad
             item_detalles.children[2].innerHTML = "Cantidad: " + item_in_cart.cantidad;
@@ -191,16 +247,22 @@ document.addEventListener("DOMContentLoaded", function() {
             container.append(item);
         }
     
+        // Se consiguen los botones de "Quitar" para configurar su comportamiento
         const buttons_remove = Array.from(document.getElementsByClassName("remove-button"));
-        
         buttons_remove.forEach((btn, index) => {
+            // Se añade un callback al botón
             btn.addEventListener("click", function(event) {
-                cart.splice(index, 1);
+                // Se elimina el elemento del carrito y se guarda en memoria
+                const element_removed = cart.splice(index, 1);
                 localStorage.setItem("cart", JSON.stringify(cart));
+
+                // Se recarga la página
                 location.reload();
             })
         })
 
+        // Los botones de "Quitar todo" y "Comprar todo" se comportan igual:
+        // Eliminan el carrito
         const button_remove_all = document.getElementsByClassName("remove-all-button")[0];
         button_remove_all.addEventListener("click", function() {
             localStorage.removeItem("cart");
@@ -215,14 +277,86 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+// detalles.html
+document.addEventListener("DOMContentLoaded", function() {
+    // Si no hay "section-detalles", no estamos en detalles.html y salímos prematuramente
+    const container = document.getElementById("section-detalles");
+    if (container === null) return;
+
+    const cart_quantity_text = document.getElementsByClassName("carrito")[0].children[0].children[1];
+    setCartQuantity(cart_quantity_text);
+
+    // Se consiguen el parámetro "id" del URL
+    const urlParams = new URLSearchParams(location.search);
+    const element_id = urlParams.get("id");
+
+    // Se busca el elemento en la colección de ropas
+    let element = null;
+    for (const element_db of db) {
+        if (element_db.id == element_id) {
+            element = element_db;
+            break;
+        }
+    }
+    if (element === null) return;
+
+    // Se asignan las características correspondientes
+
+    // Imagen
+    const image = container.children[0].children[0];
+    image.src = element.imagen;
+
+    // Detalles
+    const detalles = container.children[1];
+
+    // Título
+    detalles.children[0].innerHTML = element.nombre;
+
+    // Precio
+    detalles.children[1].innerHTML = precioToString(element.precio);
+
+    // Descripción
+    detalles.children[2].innerHTML = element.descripcion;
+
+    // Tallas
+    let talla_text = "<strong>Tallas disponibles:</strong>";
+    let count = 0;
+    for (const talla of element.tallas) {
+        talla_text = talla_text.concat(talla);
+        if (count !== element.tallas.length - 1) {
+        talla_text = talla_text.concat(" · ");
+        }
+        count += 1;
+    }
+
+    // Color
+
+    // Material
+
+    // Envío
+
+    // Se añade callback al botón "Agregar al carrito"
+    detalles.children[7].addEventListener("click", function() {
+        const item_to_push = {
+            "id": element.id,
+            "nombre": element.nombre,
+            "precio": element.precio,
+            "imagen": element.imagen,
+            "cantidad": 1
+        }
+        addToCart(item_to_push, cart_quantity_text);
+    })
+})
+
 // Solo para debug
 document.addEventListener("keydown", function(event) {
-    if (event.key === "d") {
+    if (event.key === "d") { // Elimina carrito
         localStorage.removeItem("cart");
-    } else if (event.key === "c") {
+    } else if (event.key === "c") { // Crea un carrito vacío
         localStorage.setItem("cart", JSON.stringify([]));
-    } else if (event.key === "l") {
+    } else if (event.key === "l") { // Muestra carrito en consola
         const cart = JSON.parse(localStorage.getItem("cart"));
+        console.log("Carrito");
         console.log(cart);
     }
 });
